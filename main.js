12ab07810f565a68244514eda8fedad0428917f4
Khoa <195331270+WillBeOldK@users.noreply.github.com>
2025-02-17 18:07:25 +0700

    change the js to accept the user input to create squares in the grid

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 let gridContainer = document.querySelector(".grid-container");
+let numberOfSquare = 0;
 // create small squares in grid
-for (let i = 0 ; i < 16 * 16; i ++){
+do{
+ numberOfSquare = +prompt("Enter the number of squares you want");
+}
+while (numberOfSquare < 0 || numberOfSquare > 100);
+for (let i = 0 ; i < numberOfSquare ** 2; i ++){
     let square = document.createElement("div");
     square.setAttribute("style",
-        `width : ${960 / 16 - 4 }px;
-        height : ${600 / 16 - 4 }px;
+        `width : ${960 / numberOfSquare - 4 }px;
+        height : ${600 / numberOfSquare - 4 }px;
         border : solid 2px black;
+     
         `);
+    square.addEventListener("mouseover",()=>{
+        square.setAttribute("style",
+        `width : ${960 / numberOfSquare - 4 }px;
+        height : ${600 / numberOfSquare - 4 }px;
+        border : solid 2px black;
+        background-color:red;
+        `);
+        
+    })
+   
     gridContainer.appendChild(square);
 }
